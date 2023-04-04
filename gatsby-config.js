e66ea851d1b0a2a5378f33d243dc3a27aab0d5d0
Alexander <alxndr+github@gmail.com>
2023-04-03 22:44:27 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `KGLW.today`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-sitemap", "gatsby-transformer-remark", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
