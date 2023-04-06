b401c75a3f321f6614ecd4e63dbffdb9eccec90e
Alexander <alxndr+github@gmail.com>
2023-04-05 18:29:37 -0700

    rudimentary calendar

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,7 @@
 export function dateToText(date, opts={}) {
   return date.toLocaleString('en', {numberingSystem:'latn',month:'short',day:'numeric',...opts})
 }
+
+export function dateToSlug(date) {
+  return dateToText(date).toLowerCase().replace(' ', '-')
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react'
 import {Link} from 'gatsby'
 
 import Footer from '../components/footer';
-import {dateToText} from '../helpers';
+import {dateToSlug, dateToText} from '../helpers';
 
 export const Head = () => <>
   <title>Today in King Gizzard History</title>
@@ -22,14 +22,39 @@ const IndexPage = () => {
   }, [])
   return (
     <main>
-      <noscript><strong>This site requires JavaScript.</strong></noscript>
+      
       <h1>Today in King Gizzard History</h1>
       <p>
         {day
           ? <>Today is <Link to={`/${day.toLowerCase().replace(' ','-')}`}>{day}</Link>...</>
-          : <>Loading...</>
+          : <>Loading...
+           <noscript><strong>This site requires JavaScript.</strong></noscript>
+          </>
         }
       </p>
+      <div className="calendar">
+        <h2 style={{display:'none'}}>Calendar</h2>
+        <p>Pick a different day?</p>
+        <ul style={{padding:0,display:'flex',flexFlow:'row wrap',justifyContent:'center',listStyle:'none'}}>
+          {Array(12).fill().map((_, indexMonth) => {
+            const date = new Date(2000, indexMonth, 1);
+            const monthName = dateToText(date, {month:'long'}).split(' ')[0]
+            return <li style={{flexBasis:'4em',flexGrow:0,border:'1px solid gray',padding:0,minWidth:'3em'}}>
+              <h2 style={{maxWidth:'100%',background:'gray',color:'white',margin:0,fontSize:'1em',textAlign:'center'}}>{monthName}</h2>
+              <ul style={{margin:'0.1em',padding:0,listStyle:'none',textAlign:'center'}}>
+                {Array(31).fill().map((_, indexDay) => {
+                  date.setDate(indexDay+1);
+                  global.console.log(date)
+                  if (date.getMonth() === indexMonth) {
+                    return <li><a href={`/${dateToSlug(date)}`} style={{}}>{dateToText(date)}</a></li>
+                  }
+                  return false
+                })}
+              </ul>
+            </li>
+          })}
+        </ul>
+      </div>
       <Footer/>
     </main>
   )
