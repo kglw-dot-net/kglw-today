c9a0f3e51892e87f2e73460d92a273e8a2543e54
Alexander <alxndr+github@gmail.com>
2023-04-05 17:29:33 -0700

    refactor

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,18 @@
+import {dateToText} from './src/helpers'
+
 const rootUrl = 'https://kglw.songfishapp.com';
 
 exports.createPages = async ({actions: {createPage}, graphql}) => {
 
-  for (let day = new Date(2000,0,1); day.getYear() < 101; day.setDate(day.getDate()+1)) {
+  for (let date = new Date(2000,0,1); date.getYear() < 101; date.setDate(date.getDate()+1)) {
     // iterating through the year 2000 because it has a leap day
-    const monthDay = day.toLocaleDateString('en', {numberingSystem:'latn',month:'short',day:'numeric'});
+    const monthDay = dateToText(date);
     createPage({
       path: `/${monthDay.toLowerCase().replace(' ', '-')}`,
       component: require.resolve('./src/templates/month-day.js'),
       context: {
-        month: day.getMonth() + 1, // Note: adding 1 here so that Jan=1 Dec=12 to match the data
-        day: day.getDate(),
+        month: date.getMonth() + 1, // Note: adding 1 here so that Jan=1 Dec=12 to match the data
+        day: date.getDate(),
       },
     });
   }
