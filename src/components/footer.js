17f3b9274cb2ff6ee4ab8438573adf153db915f6
Alexander <alxndr+github@gmail.com>
2023-04-07 00:03:58 -0700

    link to root page

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,6 +4,7 @@ import './footer.scss'
 
 export default function Footer() {
   return <footer className="sitefooter">
+    <nav><a href="/">Back to Today / full Calendar</a></nav>
     <p>Brought to you by <a href="http://kglw.net/?src=kglw.today&campaign=credits" target="_blank" rel="noreferrer">KGLW.net</a>!</p>
   </footer>
 }
