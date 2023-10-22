d4cfcb8b93099e7315e8b44e3d16655a63232a2e
Alexander <alxndr+github@gmail.com>
2023-10-22 12:38:36 -0700

    refactor: split `updateShows` into separate file

---
+++ updateShows.mjs
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+import fs from 'fs';
+
+console.log('updating shows...');
+
+const filename = './src/data/shows.json';
+
+const fetchCall = await fetch('https://kglw.net/api/v1/shows.json');
+const responseToJSON = await fetchCall.json();
+const content = JSON.stringify(responseToJSON.data, null, 2);
+
+fs.writeFileSync(filename, content + '\n');
+
+console.log('success!');
