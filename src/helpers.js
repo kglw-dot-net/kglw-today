709a7e02efda0781bb860ab534f18f92beb8dcd0
Alexander <alxndr+github@gmail.com>
2024-11-27 12:32:52 -0800

    excise Gatsby
    [#154]

--- src/helpers.js
+++
@@ -1,7 +0,0 @@
-export function dateToText(date, opts={}) {
-  return date.toLocaleString('en', {numberingSystem:'latn',month:'short',day:'numeric',...opts})
-}
-
-export function dateToSlug(date) {
-  return dateToText(date).toLowerCase().replace(' ', '-')
-}
