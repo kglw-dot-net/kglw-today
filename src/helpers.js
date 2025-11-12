f173b5f336a6fe7b2c98e209ec75f395ac5bb2a2
Alexander <alxndr+github@gmail.com>
2025-11-11 17:33:07 -0800

    bugfix: don't try `window` in back-end code

--- src/helpers.js
+++ src/helpers.js
@@ -9,7 +9,7 @@ export function dateToSlug(date) {
 export async function stringToSHA(message) {
   // https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest#converting_a_digest_to_a_hex_string
   const msgUint8 = new TextEncoder().encode(message) // encode as (utf-8) Uint8Array
-  const hashBuffer = await window.crypto.subtle.digest("SHA-256", msgUint8) // hash the message
+  const hashBuffer = await crypto?.subtle?.digest?.("SHA-256", msgUint8) // hash the message
   if (Uint8Array.prototype.toHex) {
     // Use toHex if supported.
     return new Uint8Array(hashBuffer).toHex() // Convert ArrayBuffer to hex string.
