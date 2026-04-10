077b32aeea79a17665aa9c77bed053e370a08991
Alexander <alxndr+github@gmail.com>
2026-04-09 23:21:25 -0700

    test: extract Calendar page integration test

---
+++ cypress/e2e/calendar.cy.ts
@@ -0,0 +1,19 @@
+describe('calendar page', () => {
+  beforeEach(() => {
+    cy.visit('/calendar')
+  })
+  it('shows a link for every day in the (leap) year', () => {
+    cy.get('.calendar')
+      .find('a')
+      .its('length')
+      .should('eq', 366);
+  })
+  it('should link to date page proper URL', () => {
+    cy.get('.calendar')
+      .find('a')
+      .contains('Jul 16')
+      .first()
+      .should('have.attr', 'href')
+      .then(href => cy.wrap(href).should('eq', '/jul-16'))
+  })
+})
